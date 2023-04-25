e51eaa753d2d76b175cf5c8b87059c92974d490f
Nick-Prog <Nickolasrodriguez98@gmail.com>
2023-04-25 18:47:22 -0500

    Final code

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def syntactic_dataset():
     Function just to test the dataset used by one of our references.
     Need to replicate a similar number of features and labels for the semantic dataset.
     '''
-    data = packages.Preprocess("C:/Users/Nick/Documents/Projects/Excel/FinalDataset/All.csv")
+    data = packages.Preprocess("C:/Users/Nicko/Documents/Cyber INT/Phishing-AutoEncoder/All.csv")
     combined = data.download()
     
     full_dataset = combined.copy() # copy dataset.
@@ -26,13 +26,47 @@ def syntactic_dataset():
     print("Negative samples:", (labels == 0).sum().sum(), "Percentage: ", ((labels == 0).sum().sum())/len(labels)*100)
     print("Total:", len(labels))
 
+    features_scaled = data.mmx_scale(features)
+
+    # Pass feature and label dataframe to starify_shuffle_split method. Obtain a training and testing set for both.
+    train_feature, train_label, test_feature, test_label = data.straify_shuffle_split(features_scaled, labels)
+
+    # Separate our training data into a training and validation set.
+    x_train, x_valid, y_train, y_valid = data.train_test_split(train_feature, train_label)
+
+    hidden = 3
+    epoch = 1
+    nodes = [x_train.shape[1] * (hidden-i) for i in range(hidden)]
+    # active = "relu"
+    active = tf.keras.layers.LeakyReLU(alpha=0.3)
+
+    # Initialize the autoencoder model
+    autoencoder = packages.Autoencoder(input_shape = (x_train.shape[1],), # Based on number of features
+                                       num_hidden_layers = hidden, 
+                                       num_nodes = nodes,
+                                       active = active) # List of how many neurons per layer
+    
+    # Train the autoencoder model
+    history = autoencoder.train(x_train, x_valid, epochs=epoch, batch_size=256)
+
+    encoder = autoencoder.get_encoder()
+    x_train_pred = encoder.predict(x_train)
+    x_valid_pred = encoder.predict(x_valid)
+
+    classifier = packages.Classifier(x_train_pred, x_valid_pred, y_train, y_valid, test_feature, test_label)
+    classifier.decision_tree(depth=5)
+    classifier.logistic_regression(max_iter=1000)
+    classifier.random_forest(estimators=100)
+    classifier.support_vector_machine(kernel="linear", regularization=1)
+    print(f"{epoch} epochs with {hidden} hidden layers for the {active} activation function. End.")
+
 def semantic_dataset():
     '''
     Function to run our semantic dataset approach through our created AE and classifiers.
     Need to run:
-    - Try running with different number of hidden layers (1-5)
+    - Try running with only one hidden layer
     - Try running with more epochs (100-500)
-    - Trying different hyper parameters (activation functions, optimizers, neurons, etc...)
+    - Try running with only LeakyReLU
     Always consider the possibiltiy of over training, if results are similar don't push it too much farther.
     '''
     data = packages.Preprocess("C:/Users/Nicko/Documents/Cyber INT/Phishing-AutoEncoder/test.csv")
@@ -58,11 +92,11 @@ def semantic_dataset():
     # Separate our training data into a training and validation set.
     x_train, x_valid, y_train, y_valid = data.train_test_split(train_feature, train_label)
 
-    hidden = 5
-    epoch = 100
+    hidden = 1
+    epoch = 500
     nodes = [x_train.shape[1] * (hidden-i) for i in range(hidden)]
-    active = "relu"
-    # active = tf.keras.layers.LeakyReLU(alpha=0.3)
+    # active = "relu"
+    active = tf.keras.layers.LeakyReLU(alpha=0.3)
 
     # Initialize the autoencoder model
     autoencoder = packages.Autoencoder(input_shape = (x_train.shape[1],), # Based on number of features
@@ -85,10 +119,10 @@ def semantic_dataset():
     print(f"{epoch} epochs with {hidden} hidden layers for the {active} activation function. End.")
 
 if __name__ == '__main__':
-    # syntactic_dataset()
     start_time = time.time()
 
-    semantic_dataset()
+    syntactic_dataset()
+    # semantic_dataset()
 
     end_time = time.time()
     elapsed_time = end_time - start_time
